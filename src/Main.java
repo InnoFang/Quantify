630337060a069855ec02410c2ce43d2913b2853d
InnoFang <q984941707@qq.com>
2017-06-05 17:15:40 +0800

    add c plus plus code for quantify

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,108 @@
 import java.io.BufferedInputStream;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by Inno Fang on 2017/6/3.
  */
 public class Main {
 
+    private static final int MAXN = 50 + 5;
+    private static final int INF  = Integer.MAX_VALUE;
+
+    private static int[]   arr          = new int[MAXN];       // 数列
+    private static int[]   sum          = new int[MAXN];       // 前缀和
+    private static int[][] dp           = new int[MAXN][MAXN]; // 保存每个自分块的最优情况
+    private static int[][] sumOfSquares = new int[MAXN][MAXN]; // 保存所有分块中数的差的平方的和
+
+    private static Scanner in = new Scanner(new BufferedInputStream(System.in));
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(new BufferedInputStream(System.in));
 
+        while (in.hasNextInt()) {
+            int cas = in.nextInt();
+            for (int i = 0; i < cas; i++) {
 
+                int count = in.nextInt();
+                int s = in.nextInt();
+
+                /* input number */
+                for (int j = 1; j <= count; j++) {
+                    arr[j] = in.nextInt();
+                }
+
+                /* sorting */
+                Arrays.sort(arr);
+
+                int sum = 0;
+                for (int j = 0; j < s; j++) {
+
+                }
+                System.out.println(sum);
+            }
+        }
         in.close();
     }
 
-    public static void absOfDiff(int num, int... args) {
-        for (int arg : args) {
-            System.out.print(Math.abs(num - arg) + ", ");
+    public static void initialData(int[] args, int val) {
+        for (int i = 0; i < args.length; i++) {
+            args[i] = val;
         }
     }
 
-    public static int averageOf(int... args) {
+}
+
+/* private static int getTheBestResultOf(List<Integer> list) {
+        return sumOfSquares(absOfDiff(averageOf(list), list));
+    }
+
+
+    public static List<Integer> absOfDiff(int aver, List<Integer> list) {
+        List<Integer> nums = new ArrayList<>();
+        for (int arg : list) {
+            nums.add(Math.abs(aver - arg));
+        }
+        return nums;
+    }
+
+    public static int averageOf(List<Integer> list) {
         double sum = 0;
-        for (int arg : args) {
+        for (int arg : list) {
             sum += arg;
         }
-        return (int) Math.floor((sum / args.length) + 0.5);
+        return (int) Math.floor((sum / list.size()) + 0.5);
     }
 
-    public static int sumOfSquares(int... args) {
+    public static int sumOfSquares(List<Integer> list) {
         int sum = 0;
-        for (int arg : args) {
+        for (int arg : list) {
             sum += arg * arg;
         }
         return sum;
     }
+*/
+/*
 
-}
+109  252  292  297  415  450  474  492  814  846  934
+ */
+
+  /*Random random = new Random();
+
+        for (int i = 0; i < 50; i++) {
+            int count = random.nextInt(51);
+            if (count < 5)
+                continue;
+            for (int j = 0; j < count; j++) {
+                System.out.print(random.nextInt(1000) + " ");
+            }
+            System.out.println();
+        }*/
+//        int res = averageOf(/*109, 252, 292, 297, 415, 450, 474, */492, 814, 846, 934);
+//        System.out.println(res);
+//        absOfDiff(772, 492, 814, 846, 934);
+//        System.out.println();
+//        System.out.println(sumOfSquares(280, 42, 74, 162));
+//        int res = averageOf(744, 755, 777, 980, 897, 902);
+//        System.out.println(res);
+//        absOfDiff(res, 1,2,3,4,5);
+//        System.out.println();
+//        System.out.println(sumOfSquares(2, 1, 0, 1, 2));
---
+++ src/code/quantify.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+#define INF (0x7ffffff)
+#define MAXN (50 + 5)
+
+using namespace std;
+
+int arr[MAXN];
+int sum[MAXN];
+int flag[MAXN][MAXN];
+int dp[MAXN][10 + 5];
+
+void get_flag(int count) {
+    for (int i = 1; i <= count; ++i) {
+        for (int j = 1; j <= count; ++j) {
+            flag[i][j] = 0;
+        }
+    }
+    for (int i = 1; i < count; ++i) {
+        for (int j = i + 1; j <= count; ++j) {
+            int aver = (int) floor(((sum[j] - sum[i - 1]) * 1.0 / (j - i + 1)) + 0.5);
+            for (int k = i; k <= j; ++k) {
+                flag[i][j] += (arr[k] - aver) * (arr[k] - aver);
+            }
+        }
+    }
+}
+
+void DP(int count, int s) {
+    for (int i = 0; i <= count; ++i) {
+        for (int j = 0; j <= s; ++j) {
+            dp[i][j] = INF;
+        }
+    }
+    dp[0][0] = 0;
+    for (int i = 1; i <= s; ++i) {
+        for (int j = 1; j <= count; ++j) {
+            for (int k = 0; k <= j - 1; ++k) {
+                dp[j][i] = min(dp[k][i - 1] + flag[k + 1][j], dp[j][i]);
+            }
+        }
+    }
+    cout << dp[count][s] << endl;
+}
+
+int cmp(const void *a, const void *b) {
+    return (*(int *) a - *(int *) b);
+}
+
+int main() {
+
+    int cas = 0;
+    while (scanf("%d", &cas) != EOF) {
+        for (int i = 0; i < cas; ++i) {
+            int count, s;
+            cin >> count >> s;
+            arr[0] = 0;
+
+            /* initial array */
+            for (int j = 1; j <= count; ++j) {
+                cin >> arr[j];
+            }
+
+            /* sorting */
+            qsort(&arr, count + 1, sizeof(int), cmp);
+
+            /* initial sum */
+            memset(sum, 0, count);
+            /* compute the sum of range from one to j (j <= count) */
+            for (int j = 1; j <= count; ++j) {
+                sum[j] = sum[j - 1] + arr[j];
+            }
+
+            /* get the flag */
+            get_flag(count);
+
+            /* dynamic programming */
+            DP(count, s);
+        }
+    }
+    return 0;
+}
